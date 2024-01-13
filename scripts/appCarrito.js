c07ad253fa2c4425e04f0ce96b86c4794016f760
dos2z <eloy.zaid@gmail.com>
2024-01-13 17:34:43 -0300

    comienzo del desarrollo del codigo del carrito. sin dibujar el html

--- scripts/appCarrito.js
+++ scripts/appCarrito.js
@@ -1,11 +1,23 @@
 
-
+const arrCarrito = [];
 
 function seleccionProducto(evt) {
+    let producto;
     if (evt.target.classList.contains("btnAgregarCarrito")) {
         evt.preventDefault();
         let productoSelect = evt.target.parentElement;
+        producto = datosProducto(productoSelect);
+        if(arrCarrito.some((prod) => (prod.nombre === producto.nombre) && (prod.peso === producto.peso))){
+            for (prod of arrCarrito){
+                if (prod.nombre === producto.nombre){
+                    prod.cantidad++
+                }
+            }
+        }else{
+            arrCarrito.push(producto)
+        }
     }
+   
 
 }
 
@@ -13,14 +25,23 @@ function datosProducto(prod){
     let nombre = prod.querySelector(".nombreProducto").textContent;
     let peso;
     let precio;
-    let cantidad = prod.querySelector(".cantidad").value;
+    if (prod.querySelector(".peso250g").checked){
+        peso = "250g";
+        precio = prod.querySelector(".precio250g").textContent;
+    } else{
+        peso = "1Kg";
+        precio = prod.querySelector(".precio1Kg").textContent;
+    }
+    let molienda = prod.querySelector(".molienda").value;
+    let cantidad = prod.querySelector("#cantidad").value;
     const producto = {
         nombre: nombre,
         formato: peso,
         molienda: molienda,
         precio: precio,
-        cantidad: cantidad,
+        cantidad: Number(cantidad),
     }
+    return producto;
 }
 
 
--- scripts/cargaProductos.js
+++ scripts/cargaProductos.js
@@ -18,25 +18,25 @@ function cargaProductos() {
             <h3 class="nombreProducto">${producto.nombre}</h3>
             <div class="formatoProducto">
                 <h4>Selecciona el peso:</h4>
-                <input type="radio" name="${producto.nombre}peso" id="${producto.id}peso250g"  class="selecPeso" checked><label for="${producto.id}peso250g">250 g</label>
-                <div class="precioProducto">$ ${producto.precio250g}</div>
-                <input type="radio" name="${producto.nombre}peso" id="${producto.id}peso1Kg" class="selecPeso" ><label for="${producto.id}peso1Kg">1 Kg</label>
-                <div class="precioProducto">$ ${producto.precio1Kg}</div>
+                <input type="radio" name="${producto.nombre}peso" id="${producto.id}peso250g"  class="selecPeso peso250g" checked><label for="${producto.id}peso250g">250 g</label>
+                <div class="precioProducto precio250g">$ ${producto.precio250g}</div>
+                <input type="radio" name="${producto.nombre}peso" id="${producto.id}peso1Kg" class="selecPeso peso1Kg" ><label for="${producto.id}peso1Kg">1 Kg</label>
+                <div class="precioProducto precio1Kg">$ ${producto.precio1Kg}</div>
             </div>
             <div class="moliendaProducto">
                 <label for="molienda">Selecciona la molienda</label>
-                <select name="" id="molienda">
-                    <option value="">En Grano</option>
-                    <option value="">Espresso</option>
-                    <option value="">Volturno/Italiana/Moka</option>
-                    <option value="">Aeropress</option>
-                    <option value="">Filtro</option>
-                    <option value="">Prensa Francesa</option>
+                <select name="" id="molienda" class="molienda">
+                    <option value="En Grano">En Grano</option>
+                    <option value="Espresso">Espresso</option>
+                    <option value="Volturno/Italiana/Moka">Volturno/Italiana/Moka</option>
+                    <option value="Aeropress">Aeropress</option>
+                    <option value="Filtro">Filtro</option>
+                    <option value="Prensa Francesa">Prensa Francesa</option>
                 </select>
             </div>
             <div class="cantidad">
                 <label for="cantidad">Cantidad</label>
-                <input type="number" name="cantidad" id="cantidad" class="cantidad" min="1" max="5" value="1">
+                <input type="number" name="cantidad" id="cantidad" min="1" max="5" value="1">
             </div>
         </div>
         <a href="#" class="btnAgregarCarrito">Añadir al carrito</a>
